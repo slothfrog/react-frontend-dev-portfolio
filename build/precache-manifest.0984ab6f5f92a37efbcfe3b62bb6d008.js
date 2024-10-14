971dc132308cb71a0ad2917d351ff03d830d6107
Connie Wang <connieniew@gmail.com>
2024-10-14 19:33:02 -0400

    add build

---
+++ build/precache-manifest.0984ab6f5f92a37efbcfe3b62bb6d008.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be0c896a44823394261b5c21c794e15d",
+    "url": "/react-frontend-dev-portfolio/index.html"
+  },
+  {
+    "revision": "22b04ca38527bdbe929d",
+    "url": "/react-frontend-dev-portfolio/static/css/2.3aee7458.chunk.css"
+  },
+  {
+    "revision": "95d7a073293de1603eb6",
+    "url": "/react-frontend-dev-portfolio/static/css/main.20e249a5.chunk.css"
+  },
+  {
+    "revision": "22b04ca38527bdbe929d",
+    "url": "/react-frontend-dev-portfolio/static/js/2.660eb29c.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/react-frontend-dev-portfolio/static/js/2.660eb29c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95d7a073293de1603eb6",
+    "url": "/react-frontend-dev-portfolio/static/js/main.82525707.chunk.js"
+  },
+  {
+    "revision": "0c71dbb3b6aa4f86725d",
+    "url": "/react-frontend-dev-portfolio/static/js/runtime-main.89d7af9d.js"
+  }
+]);
